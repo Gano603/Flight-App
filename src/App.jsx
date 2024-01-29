c21611d22f2bca0e4d566cd5d52b0c38330ad98f
mrnobo09 <shahabkhanzada999@gmail.com>
2024-01-29 18:24:13 +0500

    Basic Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router"
 import { Home } from "./pages/index"
 import NavBar from './components/navbar';
-
+import Footer from './components/footer';
 function App() {
   return (
     <>
@@ -9,6 +9,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
     </Routes>
+    <Footer/>
     </>
   );
     
---
+++ src/components/footer.jsx
@@ -0,0 +1,61 @@
+import { Link } from "react-router-dom";
+export default function Footer(){
+    const qatarAirways = ["About Us", "Awards" , "Careers" , "Press Releases" , "Sponsorship" , "Al Darb Qatarisation" , "Environmental" , "Sustainability"];
+    const groupCompanies = ["Hamad International", "Airport" , "Qatar Executive" , "Qatar Duty Free" , "Qatar Airways Cargo" , "Internal Media Services" , "Design Organisation" , "Group Companies"];
+    const businessSolution = ["Corporate Travel", "Beyond Business" , "QMICE Meetings And Events" , "Advertise With Us"];
+    const businessPartners = ["Affiliate Marketing", "E-Procurement" , "Trade Partners"];
+    const help = ["Contact Us", "Travel Alerts"];
+    return(
+        <div className="w-screen h-[36rem] bg-white grid place-items-center">
+            <div class="w-[72rem] h-[32rem]">
+                <div className="grid grid-cols-5 text-[#4a525d]">
+                    <div className="h-[30rem]">
+                        <h2>Qatar Airways</h2><br/>
+                            <ul>
+                                {qatarAirways.map((item, index) => (
+                                    <li key={index} className="underline my-1 font-semibold">{item}</li>
+                                ))}
+                            </ul>
+                    </div>
+                    <div>
+                        <h2>Group Companies</h2><br/>
+                        <ul>
+                                {groupCompanies.map((item, index) => (
+                                    <li key={index} className="underline my-1 font-semibold">{item}</li>
+                                ))}
+                        </ul>
+                    </div>
+                    <div>
+                        <h2>Business Solution</h2>
+                        <br/>
+                        <ul>
+                                {businessSolution.map((item, index) => (
+                                    <li key={index} className="underline my-1 font-semibold">{item}</li>
+                                ))}
+                        </ul>
+                    </div>
+                    <div>
+                        <h2>Business Partners</h2>
+                        <br/>
+                        <ul>
+                                {businessPartners.map((item, index) => (
+                                    <li key={index} className="underline my-1 font-semibold">{item}</li>
+                                ))}
+                        </ul>
+                    </div>
+                    <div>
+                        <h2>Help</h2>
+                        <br/>
+                        <ul>
+                                {help.map((item, index) => (
+                                    <li key={index} className="underline my-1 font-semibold">{item}</li>
+                                ))}
+                        </ul>
+                    </div>
+                </div>
+                <div></div><br/>
+                <div></div>
+            </div>
+        </div>
+    );
+}
