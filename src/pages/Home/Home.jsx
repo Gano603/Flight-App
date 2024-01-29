23bc1d7d240fe8ddcdfb5a9a8d343d70ce5fe787
mrnobo09 <shahabkhanzada999@gmail.com>
2024-01-29 23:24:59 +0500

    fixed widths

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -36,13 +36,13 @@ const Home = () => {
       </div>
       <div className="bg-white mx-auto w-[80rem]">
       </div>
-      <div className="bg-[#F2F3FA] jotia px-[5.4rem]">
-        <div className="">
-          <h1 className=' text-[#5A5B65] font-extralight md:pt-4 text-3xl'>Start Planning your next trip</h1>
-          <p className=' text-[#5A5B65] font-extralight md:py-3 text-md'>Thinking of travelling somewhere soon? Here are some options to help you get started.</p>
-          <div className='md:flex my-6 '>
+      <div className="grid place-items-center bg-[#F2F3FA] jotia px-[5.4rem]">
+        <div className="grid place-items-center">
+          <h1 className='place-self-start text-[#5A5B65] font-extralight md:pt-4 text-3xl'>Start Planning your next trip</h1>
+          <p className=' place-self-start text-[#5A5B65] font-extralight md:py-3 text-md'>Thinking of travelling somewhere soon? Here are some options to help you get started.</p>
+          <div className='grid grid-flow-col place-items-center my-6 '>
             {cardData().map((card,index)=>(
-              <div key={index} className='card bg-white md:w-[18rem] md:h-[20rem] mx-3 border-2 rounded-xl overflow-hidden'>
+              <div key={index} className='card bg-white mr-4 md:w-[18rem] md:h-[20rem] border-2 rounded-xl overflow-hidden'>
                 <img className='object-cover h-[10rem] w-[100rem]' src={card.imageUrl} alt={card.title}/>
                 <div className="grid">
                   <p className='text-3xl h-[7rem] font-thin'>{card.title}</p>
@@ -53,7 +53,7 @@ const Home = () => {
             ))}
           </div>
         </div>
-        <div className="">
+        <div className="place-self-start">
           <h1 className='jotia text-[#5A5B65] font-extralight md:pt-4 text-3xl'>Our Latest flight deals</h1>
           <div className="search-location flex py-4">
               <p>From </p>
