711232dc2085073aa784b2f1e13402462b445814
Maarij Bukhari <syedmuhammadmaarij@gmail.com>
2024-01-29 22:03:41 +0500

    Shahab

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -35,24 +35,33 @@ const Home = () => {
       </div>
       <div className="bg-white mx-auto w-[80rem]">
       </div>
-      <div className="bg-[#F2F3FA] px-[5.4rem]">
+      <div className="bg-[#F2F3FA] jotia px-[5.4rem]">
         <div className="">
-          <h1 className='jotia text-[#5A5B65] font-extralight md:pt-4 text-3xl'>Start Planning your next trip</h1>
-          <p className='jotia text-[#5A5B65] font-extralight md:py-3 text-md'>Thinking of travelling somewhere soon? Here are some options to help you get started.</p>
-          <div className='md:flex'>
+          <h1 className=' text-[#5A5B65] font-extralight md:pt-4 text-3xl'>Start Planning your next trip</h1>
+          <p className=' text-[#5A5B65] font-extralight md:py-3 text-md'>Thinking of travelling somewhere soon? Here are some options to help you get started.</p>
+          <div className='md:flex my-6 '>
             {cardData().map((card,index)=>(
-              <div key={index} className='card md:w-[18rem] mx-3 rounded-2xl'>
-                <img className='truncate' src={card.imageUrl} alt={card.title}/>
-                <h1 className='jotia'>{card.title}</h1>
-                <p className='jotia font-semibold'>{card.content}</p>
+              <div key={index} className='card bg-white md:w-[18rem] md:h-[20rem] mx-3 border-2 rounded-xl overflow-hidden'>
+                <img className='' src={card.imageUrl} alt={card.title}/>
+                <div className="mb-8">
+                  <p className='text-3xl font-thin'>{card.title}</p>
+                  <hr/>
+                  <p className='font-semibold'>{card.content}</p>
+                </div>
               </div>
             ))}
           </div>
         </div>
         <div className="">
-        <h1 className='jotia text-[#5A5B65] font-extralight md:pt-4 text-3xl'>Our Latest </h1>
+          <h1 className='jotia text-[#5A5B65] font-extralight md:pt-4 text-3xl'>Our Latest flight deals</h1>
+          <div className="search-location flex py-4">
+              <p>From </p>
+              <input type="search" placeholder="search" class="md:relative left-4 border-b-2 bg-[#F2F3FA]"/>
+              
+          </div> 
         </div>
-        <div className=""></div>
+        <div className="">
+        </div>
       </div>
       <div className="">
 
